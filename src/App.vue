ba9d93889f4fef97d19a57f0539e5e0e815d7cfe
Kimyeongho <dudgh9661@naver.com>
2022-02-11 23:06:38 +0900

    Feat : 게시물 검색 기능

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <Navigation/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import Navigation from './components/nav/Navigation.vue'
+// import Navigation from './components/nav/Navigation.vue'
 export default {
-  components: {
-    Navigation
-  }
+  // components: {
+  //   Navigation
+  // }
 }
 </script>
 
