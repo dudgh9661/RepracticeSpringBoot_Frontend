57bfa6ebdc66fce54a61a5d8c420097da1d8070a
Kimyeongho <dudgh9661@naver.com>
2022-02-26 17:54:33 +0900

    Fix : 키워드 검색시 조회 에러 해결

--- src/components/Board/BoardList.vue
+++ src/components/Board/BoardList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <navigation @searchPost="getSearchResult"> </navigation>
-    <b-table fixed responsive :items="postsList" :fields="fields"
+    <navigation @searchKeyword="getSearchResult"> </navigation>
+    <b-table
+      fixed
+      responsive
+      :items="postsList"
+      :fields="fields"
       hover
       @row-clicked="onClick"
       id="my-table"
@@ -11,41 +15,24 @@
         <col
           v-for="field in scope.fields"
           :key="field.key"
-          :style="test(field.key)"
-        >
+          :style="getCss(field.key)"
+        />
       </template>
       <template #cell(update)="row">
         <b-button
           size="sm"
-          style="background: #006400; border: 0; outline: 0;"
+          style="background: #006400; border: 0; outline: 0"
           :to="{ name: 'BoardUpdate', params: { id: row.item.id } }"
           >게시물 수정</b-button
         >
       </template>
     </b-table>
-    <!-- <b-table 
-      striped
-      hover
-      @row-clicked="onClick"
-      id="my-table"
-      :items="postsList"
-      :fields="fields"
-      small
-    >
-      <template #cell(update)="row">
-        <b-button
-          size="sm"
-          variant="primary"
-          :to="{ name: 'BoardUpdate', params: { id: row.item.id } }"
-          >게시물 수정</b-button
-        >
-      </template>
-    </b-table> -->
     <div>
       <b-button class="enroll_button" href="/enroll">게시물 등록</b-button>
     </div>
     <div>
-      <b-pagination class="pagination"
+      <b-pagination
+        class="pagination"
         v-model="currentPage"
         :total-rows="rows"
         :per-page="perPage"
@@ -73,60 +60,71 @@ export default {
         { key: "author", label: "작성자" },
         { key: "createdDate", label: "작성일" },
         { key: "liked", label: "좋아요" },
-        { key: "update", label: "" }
+        { key: "update", label: "" },
       ],
 
       postsList: [],
       perPage: 10,
       currentPage: 1,
       totalElement: 0,
-      testData: 0
+      keyword: "",
+      searchType: "",
     };
   },
   computed: {
     rows() {
-      return this.totalElement
-    }
+      return this.totalElement;
+    },
   },
   watch: {
-    currentPage (newVal) {
-      this.testData = newVal
-      this.postsList = []
-      console.log('newVal ::: ', newVal)
-      this.$axios
-      .get(this.$url + `/api/v1/posts/page/${newVal-1}`, {
-        // headers: { 'Content-Type': 'application/json' }
-      })
-      .then((response) => {
-        // this.postsList = response.data;
-        let data = response.data
-        console.log('Changed currentPage In BoardList.vue watch() ::: ', data)
-        this.postsList = data.postsList
-        this.postsList.forEach((item) => {
-          item.createdDate = this.$utils.getDateFormat(item.createdDate);
-        });
-      });
-    }
+    currentPage(newVal) {
+      this.postsList = [];
+      console.log("currentPage ::: ", this.currentPage);
+      if (!this.$utils.isEmpty(this.keyword)) {
+        console.log('keyword Search In BoardList.vue')
+        let payload = {
+          keyword: this.keyword,
+          searchType: this.searchType,
+        };
+        this.getSearchResult(payload);
+      } else {
+        console.log('Search In BoardList.vue')
+        this.$axios
+          .get(this.$url + `/api/v1/posts/page/${newVal - 1}`, {
+            // headers: { 'Content-Type': 'application/json' }
+          })
+          .then((response) => {
+            // this.postsList = response.data;
+            let data = response.data;
+            console.log(
+              "Changed currentPage In BoardList.vue watch() ::: ",
+              data
+            );
+            this.postsList = data.postsList;
+            this.postsList.forEach((item) => {
+              item.createdDate = this.$utils.getDateFormat(item.createdDate);
+            });
+          });
+      }
+    },
   },
   created() {
     this.$axios
-      .get(this.$url + `/api/v1/posts/page/${this.currentPage-1}`, {
+      .get(this.$url + `/api/v1/posts/page/${this.currentPage - 1}`, {
         // headers: { 'Content-Type': 'application/json' }
       })
       .then((response) => {
         // this.postsList = response.data;
-        let data = response.data
-        console.log('response data In BoardList.vue created() ::: ', data)
-        this.postsList = data.postsList
-        this.totalElement = data.totalElement
+        let data = response.data;
+        console.log("response data In BoardList.vue created() ::: ", data);
+        this.postsList = data.postsList;
+        this.totalElement = data.totalElement;
+
         this.postsList.forEach((item) => {
           item.createdDate = this.$utils.getDateFormat(item.createdDate);
         });
         console.log("BoardList.vue created() ::::", response);
       });
-  },
-  beforeDestroy() {
-    this.$eventBus.$off("searchPost");
   },
   methods: {
     onClick(item) {
@@ -137,31 +135,54 @@ export default {
     },
     getSearchResult(payload) {
       console.log("keyword로 검색한 결과 => ", payload);
-      this.postsList = payload.postsList;
-      this.currentPageNumber = payload.currentPageNumber;
-      this.totalPage = payload.totalPage;
+      let vueThis = this;
+      this.keyword = payload.keyword;
+      this.searchType = payload.searchType;
+
+      this.$axios
+        .get(
+          this.$url +
+            `/api/v1/posts/page/${this.currentPage - 1}/search?searchType=${
+              this.searchType
+            }&keyword=${this.keyword}`
+        )
+        .then((response) => {
+          console.log("clicksearch data : ", response);
+          let data = response.data; // 응답받은 data Object
+          console.log("keyword 검색 결과 ::: ", data);
+          this.postsList = data.postsList;
+          this.totalElement = data.totalElement;
+
+          this.postsList.forEach((item) => {
+            item.createdDate = this.$utils.getDateFormat(item.createdDate);
+          });
+          console.log("BoardList.vue getSearchResult() ::::", response);
+        })
+        .catch((error) => {
+          console.log("clickSearch() 작동 실패 ", error);
+          vueThis.$alert("검색에 실패했습니다. 관리자에게 문의해주세요.");
+        });
     },
-    test (key) {
-      console.log('key :: ', key)
+    getCss(key) {
       let css = {
-        width: ''
+        width: "",
+      };
+      if (key === "id") {
+        css.width = "10%";
+      } else if (key === "title") {
+        css.width = "30%";
+      } else if (key === "author") {
+        css.width = "20%";
+        css.variant = "success";
+      } else if (key === "createdDate") {
+        css.width = "15%";
+      } else if (key === "liked") {
+        css.width = "5%";
+      } else if (key === "update") {
+        css.width = "10%";
       }
-      if (key === 'id') {
-        css.width = '10%'
-      } else if (key === 'title') {
-        css.width = '30%'
-      } else if (key === 'author') {
-        css.width = '20%'
-        css.variant = "success"
-      } else if (key === 'createdDate') {
-        css.width = '15%'
-      } else if (key === 'liked') {
-        css.width = '5%'
-      } else if (key === 'update') {
-        css.width = '10%'
-      }
-      return css
-    }
+      return css;
+    },
   },
 };
 </script>
@@ -175,22 +196,22 @@ export default {
   box-shadow: none !important;
   border: 0;
 }
-.page-item.active .page-link {  
-    background-color: transparent;  
-    border-color: transparent;
-    outline: none;
-    text-decoration: none;
-    box-shadow: none;
-    color: green;
-    font-weight: 700;
+.page-item.active .page-link {
+  background-color: transparent;
+  border-color: transparent;
+  outline: none;
+  text-decoration: none;
+  box-shadow: none;
+  color: green;
+  font-weight: 700;
 }
-.page-item .page-link {  
-    background-color: transparent;  
-    border-color: transparent;
-    color: black;
-    outline: none;
+.page-item .page-link {
+  background-color: transparent;
+  border-color: transparent;
+  color: black;
+  outline: none;
 }
-.page-item.disabled .page-link {  
+.page-item.disabled .page-link {
   border: 0;
   color: black;
   outline: none;
@@ -199,7 +220,7 @@ export default {
   position: relative;
   left: 90%;
   border: 0;
-  background : #a6a6a6;
+  background: #a6a6a6;
   outline: none;
 }
 </style>
